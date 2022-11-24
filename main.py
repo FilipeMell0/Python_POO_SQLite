df8916123c71f298fcc9b49e5b6bdf926477d17c
Filipe <116228977+FilipeMell0@users.noreply.github.com>
2022-11-24 01:07:43 +0000

    Últimas alterações do projeto :)

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 #Importar arquivo pessoa.py no diretório model
 from model.Pessoa import Pessoa
+from database.Database import Database
+from dao.PessoaDAO import PessoaDAO
 
 #Exemplo de uso
 filipe = Pessoa(1, "Filipe Mello")
@@ -7,3 +9,15 @@ print(filipe)
 
 #Quero mostrar só nome
 print(filipe.nome)
+
+print("Daqui pra frente é o acesso ao banco")
+#Chama o objeto de banco de dados
+db = Database()
+
+#instancia o objeto
+pessoaDAO = PessoaDAO(db.conexao, db.cursor)
+
+#Quero carregar uma lista com o que veio do banco de dados
+pessoas = pessoaDAO.getAll()
+for pessoa in pessoas:
+  print(pessoa)
